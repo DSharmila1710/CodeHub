b47bbc69e6e7ee7210ffeea8fbf2ce86a6866cb3
DSharmila1710 <56862689+DSharmila1710@users.noreply.github.com>
2020-09-12 22:28:27 +0530

    Create ReverseMiddleWordsAndLetters.java

---
+++ ReverseMiddleWordsAndLetters.java
@@ -0,0 +1,18 @@
+public class ReverseMiddleWordsAndLetters {
+	/*
+	 * Reverse middle words and the characters in them
+	 */
+	
+	public static void main(String[] args) { //O(n2)
+		String str = "I am from bangalore";
+		String[] str_Arr = str.split(" ");
+		for(int i = 1;i<=(str_Arr.length-1)/2;i++) {
+			String temp = new StringBuilder(str_Arr[i]).reverse().toString();
+			str_Arr[i]=new StringBuilder(str_Arr[str_Arr.length-1-i]).reverse().toString();
+			str_Arr[str_Arr.length-1-i]=temp;
+		}
+		for(String value :str_Arr) {
+			System.out.print(value+" ");
+		}
+	}
+}
