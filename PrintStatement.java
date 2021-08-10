20f20b0443b59d1e9ab349679d82e32d3e3c5d0b
DSharmila1710 <56862689+DSharmila1710@users.noreply.github.com>
2021-08-10 19:26:12 +0530

    Updated the PrintStatement Java class

--- PrintStatement.java
+++ PrintStatement.java
@@ -3,6 +3,6 @@ package Automation.test;
 public class PrintStatement {
 
 	public static void main(String args[]) {
-		System.out.println("Hello World");
+		System.out.println("Hello Java World");
 	}
 }
